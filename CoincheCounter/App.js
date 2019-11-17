3484520f30c6794d0c40cff7700063962ecda264
MaurelT <relmaurel@gmail.com>
2019-11-17 15:30:06 +0100

    Style CSS
    Signed-off-by: MaurelT <relmaurel@gmail.com>

--- CoincheCounter/App.js
+++ CoincheCounter/App.js
@@ -45,51 +45,83 @@ export default class App extends Component<
     return (
       <DismissKeyboard>
         <View style={styles.container}>
-          <TextInput
-            style={styles.team1name}
-            value={this.state.Team1name}
-            onChange={name => this.setState({Team1name: name.nativeEvent.text})}
-          />
-          <Text style={styles.team1name}>{this.state.Team1count}</Text>
-          <TextInput
-            style={styles.team2name}
-            value={this.state.Team2name}
-            onChange={name => this.setState({Team2name: name.nativeEvent.text})}
-          />
-          <Text style={styles.team2name}>{this.state.Team2count}</Text>
-          <TextInput
-            style={styles.counter}
-            keyboardType={'numeric'}
-            numeric
-            value={this.state.CurrentRound.toString()}
-            onChange={score =>
-              this.setState({CurrentRound: Number(score.nativeEvent.text)})
-            }
-          />
-          <Button
-            title={this.state.Team1name}
-            onPress={() =>
-              this.setState({
-                Team1count: this.state.Team1count + this.state.CurrentRound,
-                CurrentRound: 0,
-              })
-            }
-          />
-          <Button
-            title={this.state.Team2name}
-            onPress={() =>
-              this.setState({
-                Team2count: this.state.Team2count + this.state.CurrentRound,
-                CurrentRound: 0,
-              })
-            }
-          />
-          <Button
-            title="End Game"
-            onPress={() =>
-              this.setState({Team1count: 0, Team2count: 0, CurrentRound: 0})
-            }
-          />
+          <View style={styles.score}>
+            <View style={styles.team1name}>
+              <TextInput
+                style={styles.textTeamName}
+                value={this.state.Team1name}
+                onChange={name =>
+                  this.setState({Team1name: name.nativeEvent.text})
+                }
+              />
+              <Text style={styles.textCounterPoint}>
+                {this.state.Team1count}
+              </Text>
+            </View>
+            <View style={styles.team2name}>
+              <TextInput
+                style={styles.textTeamName}
+                value={this.state.Team2name}
+                onChange={name =>
+                  this.setState({Team2name: name.nativeEvent.text})
+                }
+              />
+              <Text style={styles.textCounterPoint}>
+                {this.state.Team2count}
+              </Text>
+            </View>
+          </View>
+          <View style={styles.content}>
+            <View style={styles.addPoint}>
+              <TextInput
+                style={styles.counter}
+                keyboardType={'numeric'}
+                numeric
+                value={this.state.CurrentRound.toString()}
+                onChange={score =>
+                  this.setState({CurrentRound: Number(score.nativeEvent.text)})
+                }
+              />
+            </View>
+            <View style={styles.whatTeam}>
+              <View style={styles.team1}>
+                <Button
+                  color="#0984e3"
+                  title={this.state.Team1name}
+                  onPress={() =>
+                    this.setState({
+                      Team1count:
+                        this.state.Team1count + this.state.CurrentRound,
+                      CurrentRound: 0,
+                    })
+                  }
+                />
+              </View>
+              <View style={styles.team2}>
+                <Button
+                  color="#d63031"
+                  title={this.state.Team2name}
+                  onPress={() =>
+                    this.setState({
+                      Team2count:
+                        this.state.Team2count + this.state.CurrentRound,
+                      CurrentRound: 0,
+                    })
+                  }
+                />
+              </View>
+            </View>
+            <View style={styles.endGame}>
+              <Button
+                style={styles.textEndGame}
+                color="#778ca3"
+                title="End Game"
+                onPress={() =>
+                  this.setState({Team1count: 0, Team2count: 0, CurrentRound: 0})
+                }
+              />
+            </View>
+          </View>
         </View>
       </DismissKeyboard>
     );
@@ -99,16 +131,64 @@ export default class App extends Component<
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#202029',
+  },
+  score: {
+    flex: 2,
+    flexDirection: 'row',
   },
   team1name: {
-    alignSelf: 'flex-start',
+    flex: 0.5,
+    backgroundColor: '#0984e3',
   },
   team2name: {
-    alignSelf: 'flex-end',
+    flex: 0.5,
+    backgroundColor: '#d63031',
+  },
+  textTeamName: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: 'white',
+  },
+  textCounterPoint: {
+    fontSize: 25,
+    textAlign: 'center',
+    color: 'white',
+  },
+  textStyle: {
+    textAlign: 'center',
+    color: 'white',
+  },
+  content: {
+    flex: 10,
+  },
+  addPoint: {
+    flex: 8,
+    justifyContent: 'center',
   },
   counter: {
-    marginTop: '20%',
+    fontSize: 100,
+    textAlign: 'center',
+    color: 'white',
+  },
+
+  whatTeam: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  team1: {
+    flex: 0.5,
+    justifyContent: 'center',
+  },
+  team2: {
+    flex: 0.5,
+    justifyContent: 'center',
+  },
+  endGame: {
+    flex: 1,
+  },
+  textEndGame: {
+    textAlign: 'center',
+    color: '#202029',
   },
 });
